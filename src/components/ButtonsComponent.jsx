aad9d0418b187bbd66405875ed24c92194f027c8
ChristopheG80 <ch.goussard@gmail.com>
2023-10-12 08:50:20 +0200

    test

---
+++ src/components/ButtonsComponent.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Buttons extends Component{
+    
+      
+    
+    
+    
+    
+    render(){
+        return(
+            <div>
+                <p>Coucou les boutons</p>
+            </div>
+        )
+    }
+}
+
+export default Buttons;
